b6f0e5746253e2268657cfb389984ce813ee4f5d
Rupika Kapoor <rupikakapoor@users.noreply.github.com>
2018-08-01 00:54:21 +0530

    Create info.rb

---
+++ info.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'yaml'
+
+# File /etc/chef/dna.json
+dnaFile = File.read('./dna.json')
+dnaJson = JSON.parse(dnaFile)
+
+# File /etc/engineyard/instance_api.yml
+instance_apiYaml = YAML.load_file('./instance_api.yml')
+
+instances = dnaJson["engineyard"]["environment"]["instances"]
+
+printf("%-20s %-15s\n", "AWS Instance ID:",  dnaJson["engineyard"]["this"])
+printf("%-20s %-15s\n", "Role:", dnaJson["engineyard"]["environment"]["name"])
+printf("\n")
+
+# Spin through the instances and see which one we are currently using by
+# matching the instance_id from the instance_api.yml file
+instances.each do |i|
+  if i["instance_api_config"]["instance_id"] == instance_apiYaml["instance_id"]
+    printf("%-20s %-15s\n", "Public Hostname:", i["public_hostname"])
+    printf("%-20s %-15s\n", "Private Hostname:", i["private_hostname"])
+    printf("%-20s %-15s\n", "Role:", i["role"])
+  end
+end
